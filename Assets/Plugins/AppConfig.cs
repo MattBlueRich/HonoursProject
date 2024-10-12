b6fabb071b85c99fd4fed68a2eefe3a4f323919b
MattBlueRich <S240530@UOS.AC.UK>
2024-10-12 18:44:04 +0100

    Add Project Files And Cleared Errors

---
+++ Assets/Plugins/AppConfig.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// Contain configuration of a specific App.
+/// </summary>
+static class AppConfig
+{
+    public static string AppUrl              = "wss://localhost:6868";
+    public static string AppName             = "UnityApp";
+    
+    // Please fill the clientId and client Secret of your application before starting.
+    public static string ClientId            = "BTtW1AlPINvdWnfWcjGDUQBL1mxfzYPnJX6jg1x7";
+    public static string ClientSecret        = "XY9krjHsgQgagmD0ZK9vbQ6dUEQKXTtDFcSzFwd1Izq1Ele7AWGzpb4wRX0NLpA2uLDNGPqVBjRwXmXGsFQ24ASG7Gpq64G1b1czC97ByOe9UYobfUGGOk0DqoAdjwvE";
+    public static string AppVersion          = "3.3.0";
+}
