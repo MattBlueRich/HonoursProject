8110d197dc7787140128b2b1283be54cdf9500c6
MattBlueRich <S240530@UOS.AC.UK>
2024-12-23 12:17:36 +0000

    Create New Headset Input Field
    I've made the input field feel better, by having the text appear over grey slots. I will now need to work on having each part of the screen appear in focus while making changes.

--- Assets/Scripts/MenuScene/SetupUI.cs
+++ Assets/Scripts/MenuScene/SetupUI.cs
@@ -10,52 +10,28 @@ using UnityEngine.EventSystems;
 
 public class SetupUI : MonoBehaviour
 {
-    [Header("Example UI")]
-    public SimpleExample simpleExample;
-
     [Header("Setup UI")]
-    [SerializeField] private GameObject[] menuScreens; // This refers to the different UI panels the menu switches between.
     [SerializeField] private TMP_InputField headsetIDField; // This is the input field from the "Example UI" panel, for storing the inputted headset ID (headset type + ID).
-    public UnityEngine.UI.Button screen2ConfirmButton;
-    public UnityEngine.UI.Button screen2ReturnButton;
     public GameObject errorText;
 
-    [Header("Transition")]
-    [SerializeField] private Animator FadeTransitionAnimator; // This animator performs a basic fade transition between different UI panel screens.
-    [SerializeField] private float transitionTime = 1.0f;
+    private string typeSuffix = string.Empty;
+    private string idSuffix = string.Empty;
+
+    [Header("Input")]
+    public string headsetInput = string.Empty;
 
     EmotivUnityItf _eItf = EmotivUnityItf.Instance;
 
-    string headsetType;
-    EventSystem eventSystem;
-    [Header("Default Selected Buttons")]
-    [SerializeField] private GameObject screen1Default;
-    private void OnEnable()
-    {
-        eventSystem = EventSystem.current;
-    }
-
     private void Start()
     {
-        SwitchToScreen(1);
+        // Set default headset type value.
+        typeSuffix = "MN8";
+        UpdateHeadsetInput();
     }
-
     public void SetHeadsetType(string type)
     {
-        headsetType = type;
-    }
-
-    // This is used by the navigation buttons to move between the Setup UI windows.
-    public void SwitchToScreen(int screenNo)
-    {
-        switch (screenNo)
-        {
-            case 1:
-                eventSystem.SetSelectedGameObject(screen1Default);
-                break;
-        }
-
-        StartCoroutine(SwitchScreenWithFade(screenNo));
+        typeSuffix = type;
+        UpdateHeadsetInput();
     }
 
     // This is used by the headset ID input field to make all the letters capitalized.
@@ -67,20 +43,19 @@ public class SetupUI : MonoBehaviour
     // When the player enters their ID in the headset ID input field, this checks if the entered ID is valid and enables the confirm navigation button.
     public void SetHeadsetID(string ID)
     {
-        string input = headsetType + "-" + ID;
+        idSuffix = ID;
+        UpdateHeadsetInput();
 
-        _eItf.CreateSessionWithHeadset(input); // Submit headset ID.
+        _eItf.CreateSessionWithHeadset(headsetInput); // Submit headset ID.
 
         if (_eItf.headsetValid)
         {
-            screen2ConfirmButton.interactable = true;
-            screen2ReturnButton.interactable = false;
             errorText.SetActive(false);
         }
         else
         {
             errorText.SetActive(true);
-            input = headsetType;
+            idSuffix = string.Empty;
         }
     }
 
@@ -96,25 +71,8 @@ public class SetupUI : MonoBehaviour
         _eItf.LoadProfile(profileID);
     }
 
-    IEnumerator SwitchScreenWithFade(int num)
+    public void UpdateHeadsetInput()
     {
-        FadeTransitionAnimator.SetBool("FadeIn", true);
-
-        yield return new WaitForSeconds(transitionTime);
-
-        for (int i = 0; i < menuScreens.Length; i++)
-        {
-            if (i + 1 == num)
-            {
-                menuScreens[i].SetActive(true);
-                
-            }
-            else
-            {
-                menuScreens[i].SetActive(false);
-            }
-        }
-
-        FadeTransitionAnimator.SetBool("FadeIn", false);
+        headsetInput = typeSuffix + "-" + idSuffix;
     }
 }
