05b9058f84dd01156c2da16c226b52ffed6b23c0
Matt <S240530@UOS.AC.UK>
2025-02-04 21:33:50 +0000

    Animate Door Button
    The door button now has a simple animation change when in use.

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -7,16 +7,47 @@ public class ButtonScript : MonoBehaviour, IInteractable // Object is marked as 
 {
     bool canInteract = true;
 
-    public UnityEvent UE_OnButtonPressed;
 
+    public Animator buttonAnimator;
+    public bool activeOnStart = false;
+    public UnityEvent UE_OnButtonPressed;
+    private bool isActive = false;
     public bool Enabled
     {
         get { return canInteract; }
+    }
+    private void Start()
+    {
+        if (activeOnStart)
+        {
+            buttonAnimator.SetBool("On", true);
+            isActive = true;
+        }
+        else
+        {
+            buttonAnimator.SetBool("On", false);
+            isActive = false;
+        }
     }
 
     // Called by PlayerInteract.cs when the player is attempting to interact with the object.
     public void Interact(PlayerInteract interactor)
     {
         UE_OnButtonPressed.Invoke();
+        ToggleAnimation();
+    }
+
+    public void ToggleAnimation()
+    {
+        if (isActive)
+        {
+            buttonAnimator.SetBool("On", false);
+            isActive = false;
+        }
+        else
+        {
+            buttonAnimator.SetBool("On", true);
+            isActive = true;
+        }
     }
 }
